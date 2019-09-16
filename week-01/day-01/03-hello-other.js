2440b199f088eefe22111483257335ea14da83d7
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-16 18:12:17 +0800

    exercise 01-03

---
+++ week-01/day-01/03-hello-other.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+console.log('Hello, Jeff!');
+console.log('Hello, Martin!');
+console.log('Hello, Hao!');
