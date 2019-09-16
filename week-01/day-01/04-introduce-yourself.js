07b900497a835b5008eeb8e9445f9f9c245e841b
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-16 18:27:39 +0800

    exercise 04-06

---
+++ week-01/day-01/04-introduce-yourself.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+console.log("Binjie Zhang")
+console.log("23")
+console.log("1.87")
+//  Example output:
+//  John Doe
+//  31
+//  1.87
---
+++ week-01/day-01/06-coding-hours.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// An attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+console.log(6*5*17)
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+console.log((6*5*17)/(52*17)*100+'%')
