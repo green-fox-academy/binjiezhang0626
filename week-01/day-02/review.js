930c447933b1b1f694a3eefe47759c550edef37d
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-17 10:39:08 +0800

    new review code

---
+++ week-01/day-02/review.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//if
+let condition = true
+if (condition){
+    console.log('Hello if')
+} else{
+    console.log('Hello not if')
+}
+
+//for
+for (let i = 0; i <100; i++) {
+    console.log('hello')
+}
