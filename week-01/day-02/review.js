bd5f3cca84de952f8795a7d8c4a0bb9ff5994c38
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-17 11:00:06 +0800

    edited review code

--- week-01/day-02/review.js
+++ week-01/day-02/review.js
@@ -3,12 +3,47 @@
 //if
 let condition = true
 if (condition){
-    console.log('Hello if')
+    console.log("Hello if")
 } else{
     console.log('Hello not if')
 }
 
 //for
-for (let i = 0; i <100; i++) {
-    console.log('hello')
+for (let i = 0; i <5; i++) {
+    console.log("hello")
 }
+/* three way: debug; info; error
+*/
+
+//while
+// while (){
+//     console.log("hello")
+// }
+
+let j = 0;
+while(j<5){
+    console.log("hello")
+    j++
+}
+
+//continue
+let fruits = ['apple', 'banana', 'plum', 'kiwi']
+//fruits.length = 4
+for (let i = 0; i<fruits.length; i++){
+    if (fruits[i] === 'banaba'){
+        console.log('Banaba is in the frient')
+        continue
+    }
+    //other 
+}
+
+function greet(name){
+    console.log(`Greetings, dear ${name}`)
+}
+greet('Benjamin')
+
+fruits = fruits.map(function(fruit){
+    return fruit + '!'
+})
+
+fruits.forEach(greet)
