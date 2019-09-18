421692485446b6d38562b25dbc0ca6aa08c39995
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-18 10:01:08 +0800

    string

---
+++ week-01/day-03/stringManipulation.js
@@ -0,0 +1,9 @@
+let myString = 'Hello from EPAM'
+
+let splitString = myString.split(' ')
+console.log(myString.replace('Hello','Bye'))
+console.log(myString.indexOf('EPAM'))
+console.log(myString.trim())
+console.log(myString.endsWith('o'))
+console.log(myString.toUpperCase())
+console.log(myString)
