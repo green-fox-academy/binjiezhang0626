58bf934a35fecef631dec51e56fed65c45c19e4a
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-19 09:22:44 +0800

    update of exercise 14

--- week-01/day-03/14-callback-1.js
+++ week-01/day-03/14-callback-1.js
@@ -6,6 +6,9 @@ const mapWith = (array, callback) => {
   // The mapWith() function should iterate over the given array and call the callback function on every element.
   // It stores the callback return values in the output.
   // The mapWith() should return with the output array.
+  array.forEach(element => {
+    output.push(callback(element));
+})
 
   return output;
 }
@@ -20,7 +23,13 @@ console.log(mapWith([1, 2, 3], addOne));
 //expected result: [2, 3, 4]
 
 // Exercise 2:
-
+const removeSecondLetter = (word) => {
+    let result = '';
+    for (let i = 0; i < word.length; i += 2) {
+        result += word[i];
+    }
+    return result;
+}
 // Create a callback function which removes every second character from a string
 
 const words = ['map', 'reduce', 'filter'];
