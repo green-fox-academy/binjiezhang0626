b176707e0a4bfeff10ca4dfd56ff82c6a4375cab
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-24 11:26:15 +0800

    all interface

---
+++ week-02/day-02/interface.ts
@@ -0,0 +1,29 @@
+interface LinkedList {
+    add(value: string, index?: number): void;
+    get(index: number): string;
+    removeItem(value: string): void;
+    remove(index: number): string;
+    size(): number;
+  }
+
+interface Stack {
+    empty(): boolean;
+    peek(): string;
+    push(value: string): void;
+    pop(): string;
+  }
+
+interface Queue {
+    empty(): boolean;
+    peek(): string;
+    add(value: string): void;
+    remove(): string;
+  }
+interface Tree {
+    empty(): boolean;
+    add(value: string): void;
+    remove(value: string): void;
+    search(value: string): boolean
+  }
+
+export {LinkedList, Stack, Queue, Tree}
