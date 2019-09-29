ac14d7197f3d517e46db4c04b92c0fdd067a4dd6
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-29 13:54:53 +0800

    update exercise Arrow-function

--- week-02/day-06-sunday/Arrow-function.js
+++ week-02/day-06-sunday/Arrow-function.js
@@ -23,3 +23,26 @@ function frameFunction(){
 }
 
 frameFunction();
+
+// In the next exercise write two anonym functions again. This time the returned value is conditional.
+
+// If the first parameter is an empty string, it should return the second parameter. If not, it should return the two parameters joined by a single space.
+
+function multiPurposeFunction(action){
+    if (action){
+        console.log(action('John', 'Smith'));
+        console.log(action('', 'Bond'));        
+    }
+}
+
+function frameFunction(){
+   // Write your code here
+   function action(p1,p2)  {
+       if (p1 === '') {return p2}
+       else {return p1 + ' ' + p2}
+   }
+   multiPurposeFunction(action)
+
+}
+
+frameFunction();
