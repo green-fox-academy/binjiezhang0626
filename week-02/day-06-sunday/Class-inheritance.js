4aca6b9fdd18b6a925fbbce59e2e8aa2fbb06eae
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-29 15:30:25 +0800

    update exercise Class inheritance

--- week-02/day-06-sunday/Class-inheritance.js
+++ week-02/day-06-sunday/Class-inheritance.js
@@ -1,6 +1,6 @@
 // Create a Playground class that is extended from the Garden class. Give it an extra toys field which should be a string array. 
 // Give it an add(toy) function that stores the new toy in the toys array.
-
+//ES5
 // this is the base class
 function Garden (width, length) {
     this.width = width
@@ -17,9 +17,9 @@ Garden.prototype.efficiency = function() {
 }
 
 //here is class inheritance
-function playGround(width, length, toys) {
+function playGround(width, length) {
     Garden.call(this, width, length)
-    this.toys = toys
+    this.toys = []
 }
 
 playGround.prototype = Object.create(Garden.prototype)
@@ -28,6 +28,36 @@ playGround.prototype.add = function(toy) {
     this.toys.push(toy)
 }
 
-playGround1 = new playGround(1,2,[])
+
+//ES6
+class Garden {
+    constructor(width, length) {
+        this.width = width
+        this.length = length
+    }
+
+    area() {
+        return this.width * this.length
+    }
+    circumference() {
+        return 2 * (this.width + this.length)
+    }
+    efficiency() {
+        return this.area()/this.circumference()
+    }
+}
+
+class playGround extends Garden {
+    constructor(width, length) {
+        super(width,length)
+        this.toys = []
+    }
+    add(toy) {
+        this.toys.push(toy)
+    }
+}
+
+
+playGround1 = new playGround(1,2)
 playGround1.add('hello')
 console.log(playGround1)
