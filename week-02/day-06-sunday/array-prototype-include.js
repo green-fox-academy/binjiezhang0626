5cad4323d86d78b9c8b72c7ee2a3c2d30a510cba
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-09-30 15:49:54 +0800

    change the uncorrect understanding and modified exercise array-prototype-include

--- week-02/day-06-sunday/array-prototype-include.js
+++ week-02/day-06-sunday/array-prototype-include.js
@@ -5,7 +5,7 @@ if (arr.indexOf(2) !== -1){
 if (arr.includes(2)){
     console.log("We found 2 in the array with ES7 tools.");
 }
-
+// After that create a function that takes two arrays and returns those numbers which are not present in both arrays.
 class set {
     constructor(length = 0) {
         this.length = length
@@ -29,17 +29,20 @@ class set {
         }
     }
 
-
-}
-function remove(array1,array2) {
-    let removeArr = []
-    let concatArr = array1.concat(array2)
-    concatArr .forEach( element => {
-        if (!removeArr.includes(element)) {
-            removeArr.push(element)
+    check(array1, array2) {
+        let result = []
+        let concatArr = array1.concat(array2)
+        for (let e of this.array) {
+            if (!concatArr.includes(e)) {
+                result.push(e)
+            }
         }
-    })
-    return removeArr
+        return result
+    }
 }
 
-console.log(remove([1, 2, 3], [3, 4, 5, 6]))
+let set1 = new set()
+
+set1.add(1).add(2).add(3).add(4)
+console.log(set1)
+console.log(set1.check([1,2],[2,3]))
