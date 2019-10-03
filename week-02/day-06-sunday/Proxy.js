34ea8469ab9d60ef975876395cdbaffd89c7bb52
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-03 22:44:29 +0800

    update previous exercise

--- week-02/day-06-sunday/Proxy.js
+++ week-02/day-06-sunday/Proxy.js
@@ -1,25 +1,19 @@
-const target = {a:1,b:1,c:1};
+const target = { a: 1, b: 1, c: 1 }
+
 const handler = {
-    get: function(targetObject, field){
-        if (field === 'a' || field === 'b' || field === 'c'){
-            return targetObject[field];
-        }
-        if (field === 'circ'){
-            return targetObject.a + targetObject.b + targetObject.c;
-        }
-        return undefined;
-    },
-    set: function(targetObject, field, value){
-        if (field === 'a' || field === 'b' || field === 'c'){
-            return targetObject[field] = value;
-        }
+  get: function(target, field) {
+    if (field === 'nextId') {
+      if (target.hasOwnProperty('nextId')) {
+        target['nextId']++
+      } else {
+        target['nextId'] = 1
+      }
+      return target['nextId']
     }
-};
-const proxyObject = new Proxy(target, handler);
-proxyObject.a = 3;
-proxyObject.b = 4;
-proxyObject.c = 5;
-console.log(proxyObject.circ);
-proxyObject.x = 8;
-console.log(proxyObject.x);
-console.log(target.x);
+  }
+}
+
+const proxyObject = new Proxy(target, handler)
+console.log(proxyObject.nextId)
+console.log(proxyObject.nextId)
+console.log(proxyObject.nextId)
