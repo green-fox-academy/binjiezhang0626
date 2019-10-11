23c04eef218718b8a1ed6e420dcd41366206eb38
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-11 15:57:44 +0800

    update W3D5 exercise new-york-times

--- week-03/day-05/xhr/giphy-api/giphy.js
+++ week-03/day-05/xhr/giphy-api/giphy.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const appendUl = number => {
     let ul = document.createElement('ul')
     document.body.appendChild(ul)
@@ -46,6 +48,6 @@ const handleClick = (height, width, url) => {
   }
   dataRequest.open(
     'GET',
-    `https://api.giphy.com/v1/gifs/trending?api_key=gAIr1h0t27DN4KP7zANsfZxc0ZMt9kbs&limit=16&rating=PG-13`
+    'https://api.giphy.com/v1/gifs/trending?api_key=gAIr1h0t27DN4KP7zANsfZxc0ZMt9kbs&limit=16&rating=PG-13'
   )
   dataRequest.send()
---
+++ week-03/day-05/xhr/new-york-times/new-york-times.js
@@ -0,0 +1,54 @@
+'use steict'
+
+const createAnArticle = (headline, snippet, date, link) => {
+  let article = document.createElement('article')
+  let head = document.createElement('h1')
+  head.textContent = headline
+  article.appendChild(head)
+
+  let snip = document.createElement('p2')
+  snip.textContent = 'Snippet: ' + snippet
+  article.appendChild(snip)
+
+  let pubDate = document.createElement('p')
+  pubDate.textContent = 'Publication date: ' + date
+  article.appendChild(pubDate)
+
+  let url = document.createElement('a')
+  url.textContent = link
+  url.href = link
+  article.appendChild(url)
+
+  return article
+}
+
+const appendArticlesToUl = articles => {
+  let ul = document.createElement('ul')
+  ul.style.listStyle = 'none'
+  document.body.appendChild(ul)
+
+  articles.forEach(article => {
+    let li = document.createElement('li')
+    li.appendChild(article)
+    ul.appendChild(li)
+  })
+}
+
+const dataRequest = new XMLHttpRequest()
+
+dataRequest.onreadystatechange = () => {
+  if (dataRequest.readyState === 4) {
+    docs = JSON.parse(dataRequest.response).response.docs
+    let articles = []
+    docs.forEach(doc => {
+      let { headline, snippet, pub_date, web_url } = doc
+      let article = createAnArticle(headline.main, snippet, pub_date, web_url)
+      articles.push(article)
+    })
+    appendArticlesToUl(articles)
+  }
+}
+dataRequest.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=the+moon+landing+by+Apollo+11&api-key=ok8dG2DZLb2WbRmLU6S2sYHI2c7Sl3mQ')
+dataRequest.send()
+
+console.log(dataRequest)
