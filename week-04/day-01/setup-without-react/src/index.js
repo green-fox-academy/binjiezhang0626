95dd4cdd5f14e8b318203b22e675b4cb23d97153
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-14 15:17:58 +0800

    update W4D1 setup-without-react

---
+++ week-04/day-01/setup-without-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import FirstComponent from './first-component'
+import SecondComponent from './second-component'
+
+let element = <SecondComponent items={[ 'a', 'b', 'c' ]} />;
+
+ReactDOM.render(element, document.getElementById('react-container'));
