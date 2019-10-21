5f887389d513df9273c04636feb6fdc72fc07810
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-21 15:57:48 +0800

    update W5D1 plain-redux use combineRoducers DONE

--- week-05/day-01/plain-redux/src/index.js
+++ week-05/day-01/plain-redux/src/index.js
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import reducer from './reducer'
+import { createStore, combineReducers } from 'redux';
+import counterReducer from './reducers/counterReducer'
+import tagReducer from './reducers/tagReducer';
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({ counter: counterReducer, tags: tagReducer })
+const store = createStore(rootReducer)
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState())
-  })
+})
 
-store.dispatch({type: 'SET', amount: 6});
-store.dispatch({type: 'INCREASE'});
-store.dispatch({type: 'INCREASE', amount: 2});
-store.dispatch({type: 'RESET'});
-store.dispatch({type: 'INCREASE'});
-store.dispatch({type: 'DECREASE', amount: 4});
+store.dispatch({ type: 'SET', amount: 6 });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'INCREASE', amount: 2 });
+store.dispatch({ type: 'RESET' });
+store.dispatch({ type: 'INCREASE' });
+store.dispatch({ type: 'DECREASE', amount: 4 });
+store.dispatch({ type: 'ADD_TAG', tag: 'blue' });
+store.dispatch({ type: 'ADD_TAG', tag: 'bold' });
+store.dispatch({ type: 'ADD_TAG', tag: 'hipster' });
+store.dispatch({ type: 'REMOVE_TAG', tag: 'blue' });
+store.dispatch({ type: 'ADD_TAG', tag: 'bold' });
+store.dispatch({ type: 'REMOVE_TAGS' });
 
 unsubscribe()
 ReactDOM.render(<App />, document.getElementById('root'));
