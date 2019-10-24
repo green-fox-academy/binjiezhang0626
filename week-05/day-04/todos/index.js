6aed74462570489bcce52df769047cf673872f1c
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-24 14:53:40 +0800

    update W5D4 exercise todos

---
+++ week-05/day-04/todos/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+]
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Todos',
+        todos
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
