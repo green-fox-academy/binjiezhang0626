c6e3efdc27f40b68f5c1507fce4fa2d03092fbea
Binjie Zhang <im.binjie.zhang@outlook.com>
2019-10-28 15:58:45 +0800

    update W6D1 Movie-Rating Query Exercises

---
+++ week-06/day-01/01.sql
@@ -0,0 +1,26 @@
+-- q1 --
+SELECT title FROM Movie
+WHERE director = "Steven Spielberg";
+-- q2 --
+SELECT DISTINCT Movie.year from Rating, Movie
+WHERE Movie.mID = Rating.mID AND stars >= 4
+ORDER BY Movie.year ASC;
+-- q3 --
+SELECT DISTINCT Movie.title FROM Movie,Rating
+WHERE Movie.mID NOT IN (SELECT DISTINCT mID FROM rating);
+-- q4 --
+SELECT DISTINCT Reviewer.name FROM Reviewer, Rating
+WHERE Reviewer.rID = Rating.rID AND Rating.ratingDate is NULL;
+-- q5 --
+SELECT
+  DISTINCT Reviewer.name AS REVIEWER_NAME, 
+  Movie.title AS MOVIE_TITLE, 
+  Rating.stars AS STARS,
+  Rating.ratingDate AS DATE
+FROM Movie, Reviewer, Rating
+WHERE
+  Rating.rID = Reviewer.rID AND Rating.mID = Movie.mID
+ORDER BY
+  Reviewer.name,
+  Movie.title,
+  Rating.stars;
